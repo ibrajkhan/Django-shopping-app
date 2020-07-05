03bff4f7fec4d398e483ebf28aba4b5a980d7d1f
IbrajKhan <ibraj.grd@gmail.com>
2020-07-05 20:23:54 +0530

    Added templates index.html

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 def index(request):
-    return HttpResponse("hello world !")
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
 
 
 def new_page(request):
